517de5d1ab492ef560e9e823f6a1903fb5a7f874
SuryaNeil Ahuja <suryaneil.ahuja@berkeley.edu>
2023-09-25 15:07:45 -0700

    Rearranged files

---
+++ Demos/list_demo_empty.py
@@ -0,0 +1,62 @@
+
+# Let's start with an empty list:
+
+
+
+#Next, let's add the following fruits to the list: 'apple', 'banana', and 'kiwi'.
+
+
+
+#Print the second element in the list (which index would that be?)
+
+
+
+#Now add another 'banana' to the end of the list, and replace the first one with 'cherry'.
+#See if you can do this without actually typing out 'banana' in your code.
+
+
+
+#Sort the list in alphabetical order (see built in methods)
+
+
+
+#Print the length of fruits
+
+
+
+
+
+
+#SECOND DEMO
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#Slice the list to include only the integers from 2 to 9 (inclusive) and print its length.
+
+
+#Now create a new list with all of the odd numbers in nums in decreasing order
+
+
+
+# Creating two sample lists with different types of elements
+
+
+# Concatenating lists using the '+' operator
+
+
+
+# Printing the concatenated list
+
+
+# Creating another sample list
+
+
+
+# Concatenating lists using the 'extend()' method
+
+
+
+# Printing the updated list1 after concatenation using 'extend()'
+
+
+
+
